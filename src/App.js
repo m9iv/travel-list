249a01d846fa0b7d175ce4e9ff6c61bb227efc08
m9iv <aethawalka@gmail.com>
2024-08-27 18:10:30 +0300

    Add basic elements and logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 2, description: "Headphones", quantity: 1, packed: false },
+];
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>🏕️ Far Away 🎒</h1>;
+}
+
+function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 👣 trip?</h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item" />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      {initialItems.length === 0 ? (
+        <span>
+          Add some items 👆 <br /> And you will see it right here.
+        </span>
+      ) : (
+        <ul>
+          {initialItems.map((item, i) => (
+            <Item item={item} key={i} />
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+function Item({ item }) {
+  console.log(item);
+  return (
+    <li>
+      <input type="checkbox" defaultChecked={item.packed} />
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.description} ({item.quantity})
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <span>
+        ✌️ You have X items on your list, and you already packed X (X%)
+      </span>
+    </footer>
+  );
+}
